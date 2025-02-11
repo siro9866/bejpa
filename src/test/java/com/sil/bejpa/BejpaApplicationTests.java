10f28d8a18fc30e03921ebfa464f4f03c378b582
siro9866 <siro9866@gmail.com>
2025-02-11 13:12:41 +0900

    Update BejpaApplicationTests.java

--- src/test/java/com/sil/bejpa/BejpaApplicationTests.java
+++ src/test/java/com/sil/bejpa/BejpaApplicationTests.java
@@ -3,10 +3,10 @@ package com.sil.bejpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BejpaApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
